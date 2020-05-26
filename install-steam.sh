090df81b5ec75b17fb496c99ba0bda29cd80b4dd
Joseph Kramer <joseph.ryan.kramer@gmail.com>
2020-05-26 17:52:54 -0400

    automatically say yes to apt-get

--- install-steam.sh
+++ install-steam.sh
@@ -1,1 +1,1 @@
-sudo apt-get update && sudo apt-get install software-properties-common && sudo apt-add-repository non-free && sudo apt-add-repository contrib && sudo dpkg --add-architecture i386 && sudo apt-get update && sudo apt-get install steam
+sudo apt-get update && sudo apt-get -y install software-properties-common && sudo apt-add-repository non-free && sudo apt-add-repository contrib && sudo dpkg --add-architecture i386 && sudo apt-get update && sudo apt-get -y install steam
