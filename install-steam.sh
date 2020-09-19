467172b75f04e4ca20d37f6fa0c6dffa702dcb5d
Joseph Kramer <joseph.ryan.kramer@gmail.com>
2020-09-19 14:37:50 -0400

    make script more readable

--- install-steam.sh
+++ install-steam.sh
@@ -1,1 +1,7 @@
-sudo apt-get update && sudo apt-get -y install software-properties-common && sudo apt-add-repository non-free && sudo apt-add-repository contrib && sudo dpkg --add-architecture i386 && sudo apt-get update && sudo apt-get -y install steam
+sudo apt-get update && \
+sudo apt-get -y install software-properties-common && \
+sudo apt-add-repository non-free && \
+sudo apt-add-repository contrib && \
+sudo dpkg --add-architecture i386 && \
+sudo apt-get update && \
+sudo apt-get -y install steam
