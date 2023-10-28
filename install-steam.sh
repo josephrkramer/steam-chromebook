a16fbc19f6b825f90bda3a3278e9477249e092d9
Joseph Kramer <joseph.ryan.kramer@gmail.com>
2023-10-28 10:54:40 -0400

    add lsof to install-steam.sh

--- install-steam.sh
+++ install-steam.sh
@@ -4,4 +4,4 @@ sudo apt-add-repository non-free && \
 sudo apt-add-repository contrib && \
 sudo dpkg --add-architecture i386 && \
 sudo apt-get update && \
-sudo apt-get -y install steam
+sudo apt-get -y install lsof steam
